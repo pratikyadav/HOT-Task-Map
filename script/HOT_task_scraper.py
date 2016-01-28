4ed81178877416a043c823e23235456e1883e525
Pratik <pratik@mapbox.com>
2016-01-28 14:41:43 +0530

    some code cleaning

--- script/HOT_task_scraper.py
+++ script/HOT_task_scraper.py
@@ -1,25 +1,31 @@
 import requests
+import time
+#total number of tasks as on 5th January is 1418.
+#Change this to the current task number
 
-task=1
+for task in range(1,1419):
 
-while task<1418:
+    response = requests.get("http://tasks.hotosm.org/project/"+str(task)+".json")
+    print task
+    print response
+    
+    # one second pause to make sure we do not hit api hard
+    time.sleep(1)
+    
+    # reponse status will be 200 i.e okay if the task exists
+    if response.status_code==200:
 
-    r = requests.get("http://tasks.hotosm.org/project/"+str(task)+".json")
-    print r
-    #print type(r)
-    if str(r)=="<Response [200]>":
+        body=response.text
+        
 
-        body=r.text
-        print task
-
-
-        f = open('pypy/'+str(task)+'.json', 'w')
+        # save the file in test directory as <task number>.json
+        f = open('test/'+str(task)+'.json', 'w')
         f.write(body)
-        #task=task+1
+        
 
 
         f.close()
-    task=task+1
+    
 
 
 
